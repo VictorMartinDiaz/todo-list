a66181555409817843ed4c609a9562c13e5b9a43
Victor <viktorvk@gmail.com>
2021-02-09 23:59:02 +0100

    listado en formato mas legible

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const main = async () => {
             break;
 
             case '2':
-                console.log(tareas.listadoArr);
+                tareas.listadoCompleto();
             break;
 
         }
--- models/tareas.js
+++ models/tareas.js
@@ -37,6 +37,18 @@ class Tareas {
         });
     }
 
+    listadoCompleto () {
+        const idx = `${i+1}`.cyan;
+        const {desc, completadoEn} = tarea;
+        const estado = (completadoEn) ? 'Completada'.green : 'Pendiente'.red;
+
+        this.listadoArr.forEach((tarea, i) => {
+            this._listado[tarea.id] = tarea;
+            console.log(`${idx} ${desc} :: ${estado}`);
+        });
+
+    }
+
 }
 
 module.exports = Tareas;
